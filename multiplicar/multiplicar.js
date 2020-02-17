d18fb42a00ef32219192badbae2678cccfd89fb6
cynhv <cynthia.hidalgo18@gmail.com>
2020-02-17 11:49:21 -0300

    Commit Inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,47 @@
+//requires
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite = 10) => {
+
+    console.log('=================');
+    console.log(`tabla de ${base}`.green);
+    console.log('=================');
+
+
+    //muestra en consola la tabla pero depende del limite
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${base} * ${i} = ${base * i}\n`);
+    }
+
+}
+
+
+let crearArchivo = (base, limite = 10) => {
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido "${base}" no es un número`);
+            return;
+        }
+
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            data += `${base} * ${i} = ${base * i}\n`;
+        }
+
+        fs.writeFile(`tabla-${base}-al-${limite}.txt`, data, (err) => {
+
+            if (err) reject(err);
+            else
+                resolve(`tabla-${ base }-al-${limite}.txt`.yellow);
+
+        });
+    });
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
